1fd18061381552f6287ad49fcdc785c8e5362722
softhe <suport.softhe@gmail.com>
2025-08-26 19:45:54 +0200

    d

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Replace with your GitHub repo name, e.g. "/crosshair-tools/"
-const base = "/crosshair-tools/";
+const base = "/delli.cc/";
 
 export default defineConfig({
   plugins: [react()],
